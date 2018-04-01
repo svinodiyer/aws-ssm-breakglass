7738e6fa0a8cdfb3d1ade118bf3d98cb06a11397
svinodiyer <svinodiyer@gmail.com>
2018-04-01 14:31:03 +1000

    Create lambda-test-event-linux-enable.js

---
+++ lambda-test-event-linux-enable.js
@@ -0,0 +1,15 @@
+{
+  "account_id": "<<aws account number>>",
+  "role_name": "<<ssm role to execute break glass>>",
+  "account_region": "<<aws region>>",
+  "platform_type": "Linux",
+  "command_type": "ENABLE",
+  "tag_value": "None",
+  "instance_id_list": [
+    "<aws instanceid>>"
+  ],
+  "elb_name": "None",
+  "elb_type": "None",
+  "username": "breakglass"
+}
+
