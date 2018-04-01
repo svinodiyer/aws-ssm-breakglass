e4e0c45ddc70e0cf0430a2aea0358804185918c7
svinodiyer <svinodiyer@gmail.com>
2018-04-01 14:33:32 +1000

    Create lambda-test-event-windows-enable.js

---
+++ lambda-test-event-windows-enable.js
@@ -0,0 +1,14 @@
+{
+  "account_id": "<<aws account number>>",
+  "role_name": "<<ssm role to execute break glass>>",
+  "account_region": "<<aws region>>",
+  "platform_type": "Windows",
+  "command_type": "ENABLE",
+  "tag_value": "None",
+  "instance_id_list": [
+    "<aws instanceid>>"
+  ],
+  "elb_name": "None",
+  "elb_type": "None",
+  "username": "breakglass"
+}
